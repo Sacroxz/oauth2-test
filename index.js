e6951943c4e2a4919f4373e785b278e945bb3744
Sacroxz <ed.navarrol@duocuc.cl>
2022-12-01 19:39:57 -0300

    Changed callback URL

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5001/google/callback",
+  callbackURL: "https://oauth2-test-nine.vercel.app/google/callback",
   passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
